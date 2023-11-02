53e2b506e2f4865ec422a775725413f7ae06d184
Jason Rowland <jay_r83@outlook.com>
2023-11-02 13:33:21 -0400

    altered function iWLO

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
 
-function introductionWithLanguageOptional(name, language){
-    if(language){
-        return `Hi, my name is ${name} and I am learning to program in ${language}.`
-    }
-
-    return `Hi, my name is ${name} and I am learning to program in JavaScript.`
+function introductionWithLanguageOptional(name, language = 'JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
